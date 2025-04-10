61c427cd33efbbaa69fa3e1517a816c78be87df9
MarcusDeLuca <made1570@colorado.edu>
2025-04-09 23:29:37 -0600

    python dependencies

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
-python3 ml_server.py &
 
-# Wait a bit to make sure it's running
+python3 ml_server.py &
+PYTHON_PID=$!
+
 sleep 1
 
-# Start Node server
-npx nodemon server.js
+npx nodemon server.js &
+NODE_PID=$!
+
+cleanup() {
+  echo "Shutting down servers..."
+  kill $PYTHON_PID
+  kill $NODE_PID
+  exit
+}
+
+trap cleanup SIGINT
+
+wait $NODE_PID
